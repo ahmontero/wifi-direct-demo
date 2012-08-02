cd3318e2974b26358c73aef7b104d021b1a3d47b
ahmontero <antonio.hinojo.montero@gmail.com>
2012-08-02 16:52:42 +0200

    wifidirectdemo

---
+++ src/com/example/android/wifidirect/Utils.java
@@ -0,0 +1,96 @@
+package com.example.android.wifidirect;
+
+import java.io.BufferedReader;
+import java.io.FileReader;
+import java.io.IOException;
+import java.net.Inet4Address;
+import java.net.Inet6Address;
+import java.net.InetAddress;
+import java.net.NetworkInterface;
+import java.net.SocketException;
+import java.util.Enumeration;
+
+import android.util.Log;
+
+public class Utils {
+
+	public static String getIPFromMac(String mac_address) {
+		/*
+		 * method modified from
+		 * http://www.flattermann.net/2011/02/android-howto-find-the-hardware-mac-address-of-a-remote-host/
+		 * 
+		 * */
+		BufferedReader br = null;
+		try {
+			br = new BufferedReader(new FileReader("/proc/net/arp"));
+			String line;
+			while ((line = br.readLine()) != null) {
+				String[] splitted = line.split(" +");
+				if (splitted != null && splitted.length >= 4) {
+					// Basic sanity check
+					String mac = splitted[3];
+					if (mac.matches(mac_address)) {
+						return splitted[0];
+					}
+				}
+			}
+		} catch (Exception e) {
+			e.printStackTrace();
+		} finally {
+			try {
+				br.close();
+			} catch (IOException e) {
+				e.printStackTrace();
+			}
+		}
+		return null;
+	}
+
+
+	public static String getLocalIPAddress() {
+		/*
+		 * modified from:
+		 * 
+		 * http://thinkandroid.wordpress.com/2010/03/27/incorporating-socket-programming-into-your-applications/
+		 * 
+		 * */
+		try {
+			for (Enumeration<NetworkInterface> en = NetworkInterface.getNetworkInterfaces(); en.hasMoreElements();) {
+				NetworkInterface intf = en.nextElement();
+				for (Enumeration<InetAddress> enumIpAddr = intf.getInetAddresses(); enumIpAddr.hasMoreElements();) {
+					InetAddress inetAddress = enumIpAddr.nextElement();
+					if (!inetAddress.isLoopbackAddress()) {
+						if (inetAddress instanceof Inet4Address) { // fix for Galaxy Nexus. IPv4 is easy to use :-)
+							return getDottedDecimalIP(inetAddress.getAddress());
+						}else if(inetAddress instanceof Inet6Address){
+							return inetAddress.getHostAddress().toString(); // Galaxy Nexus returns IPv6
+						}
+					}
+				}
+			}
+		} catch (SocketException ex) {
+			Log.e("AndroidNetworkAddressFactory", "getLocalIPAddress()", ex);
+		} catch (NullPointerException ex) {
+			Log.e("AndroidNetworkAddressFactory", "getLocalIPAddress()", ex);
+		}
+		return null;
+	}
+
+	private static String getDottedDecimalIP(byte[] ipAddr) {
+		/*
+		 * ripped from:
+		 * 
+		 * http://stackoverflow.com/questions/10053385/how-to-get-each-devices-ip-address-in-wifi-direct-scenario
+		 * 
+		 * */
+		//convert to dotted decimal notation:
+		String ipAddrStr = "";
+		for (int i=0; i<ipAddr.length; i++) {
+			if (i > 0) {
+				ipAddrStr += ".";
+			}
+			ipAddrStr += ipAddr[i]&0xFF;
+		}
+		return ipAddrStr;
+	}
+}
